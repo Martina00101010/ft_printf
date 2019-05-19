e6519ac85872c3d1c5075400f97b4c7a272d83fb
Pypar Berge <pberge@oa-g5.21-school.ru>
2019-05-19 20:53:09 +0300

    basic text and strings handle

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pberge <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 21:19:01 by pberge            #+#    #+#             */
-/*   Updated: 2019/05/18 17:30:09 by pberge           ###   ########.fr       */
+/*   Updated: 2019/05/19 20:00:42 by pberge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,71 +21,86 @@ static int	parse_text(char **s, char **to_print, int size)
 	int		len;
 	char	*tmp;
 
+	printf("    len %zu  |%s|\n", ft_strlen(*s), *s);
 	len = 0;
 	while ((*s)[len] != '%' && (*s)[len] != '\0')
 		len++;
 	tmp = *to_print;
-//	*to_print = ft_strnew(size + len + 1);
-//	*to_print = ft_strjoin();
-//	free(tmp);
+	*to_print = ft_strnew(size + len);		// NULL validation
+	ft_strcat(*to_print, tmp);
+	ft_strncat(*to_print + size, *s, len);
+	free(tmp);
 	*s += len;
 	return (len);
 }
+
 /*
 static int	parse_backlash()
 {
 	return (1);
 }
+*/
 
-
-static int	parse_param(char *s, va_list ap, char **to_print)
+static int	parse_param(char **s, char **to_print, va_list ap, size_t size)
 {
 	int		len;
-	int		d;
-	char	*e;
-	char	c;
+//	int		d;
+//	char	c;
+	char	*sparam;
+	char	*tmp;
 
 	len = 0;
-	if (*s == 's')
+	if (*(*s + 1) == 's')
 	{
-		e = va_arg(ap, char *);
-		printf("%s\n", e);
+		sparam = va_arg(ap, char *);
+		len = ft_strlen(sparam);
+		tmp = *to_print;
+		*to_print = ft_strnew(size + len);
+		ft_strcat(*to_print, tmp);
+		ft_strcat(*to_print + size, sparam);
+		free(tmp);
+		*s += 2;
 	}
-	else if (*s == 'd')
+	printf("pointer |%s|\n", *s);
+/*	else if (*s == 'd')
 	{
 		d = va_arg(ap, int);
-		printf("%i\n", d);
+//		printf("%i\n", d);
 	}
 	else if (*s == 'c')
 	{
 		c = va_arg(ap, int);
-		printf("%i %zu\n", c, sizeof(c));
+//		printf("%i %zu\n", c, sizeof(c));
 	}
-	return (len);
+*/	return (len);
 }
-*/
 
 int			ft_printf(char *s, ...)
 {
 	va_list	ap;
-	size_t	len;
+	int		len;
 	char	*to_print;
-//	char	*tmp;
 
 	va_start(ap, s);
 	len = 0;
 	to_print = ft_strnew(len);
 	while (*s)
 	{
+		printf("before |%s| |%s|\n", to_print, s);
 //		if (*s == '\')
 //			parse_backslash();
-//		else if (*s == '%')
-			//len += parse_param(s + 1, ap, char **to_print);
-//		else
+		if (*s == '%')
+		{
+			len += parse_param(&s, &to_print, ap, len);
+	printf("|%s|len %i\n", to_print, len);
+		}
+		else
+		{
 			len += parse_text(&s, &to_print, len);
-		s++;
+	printf("|%s|len %i\n", to_print, len);
+		}
+		printf("after  |%s| |%s|\n\n", to_print, s);
 	}
-//	write(1, to_print, len);
-	printf("%s\n", to_print);
+	write(1, to_print, len);
 	return (len);
 }
