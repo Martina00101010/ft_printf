4b88af2da228ddf50408b5abf8e8f5eaa6ff98c8
Pypar Berge <pberge@at-i6.21-school.ru>
2019-05-23 22:09:12 +0300

    check fixed

--- libft/ft_strnew.c
+++ libft/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: pberge <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 16:34:49 by pberge            #+#    #+#             */
-/*   Updated: 2019/05/18 16:10:49 by pberge           ###   ########.fr       */
+/*   Updated: 2019/05/23 21:22:21 by pberge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_strnew(size_t size)
 {
 	char	*s;
 
-	if (size < size + 1)
+	if (size > size + 1)
 		return (NULL);
-	if (!(s = (char *)malloc(sizeof(char) * (size + 1))));
+	if (!(s = (char *)malloc(sizeof(char) * (size + 1))))
 		return (NULL);
 	ft_memset(s, '\0', size + 1);
 	return (s);
