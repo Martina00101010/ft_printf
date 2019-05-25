7d70f1f9fe23a335f96a7f0b587e8a3cf954081c
Pypar Berge <pberge@mi-n6.21-school.ru>
2019-05-25 16:03:42 +0300

    + parse_string

--- inc/libftprintf.h
+++ inc/libftprintf.h
@@ -6,13 +6,16 @@
 /*   By: pberge <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 16:32:01 by pberge            #+#    #+#             */
-/*   Updated: 2019/05/12 16:32:52 by pberge           ###   ########.fr       */
+/*   Updated: 2019/05/25 12:57:36 by pberge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+#include <stdarg.h>
+
 int		ft_printf(char *s, ...);
+int		parse_string(va_list ap, char **to_print, int size);
 
 #endif
