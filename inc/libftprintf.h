a03f31351b7cf567ec6ab6180e4ee4ad94dccff5
Pypar Berge <pberge@mi-n6.21-school.ru>
2019-05-25 18:57:25 +0300

    + structure t_flags

--- inc/libftprintf.h
+++ inc/libftprintf.h
@@ -6,16 +6,25 @@
 /*   By: pberge <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 16:32:01 by pberge            #+#    #+#             */
-/*   Updated: 2019/05/25 12:57:36 by pberge           ###   ########.fr       */
+/*   Updated: 2019/05/25 17:48:39 by pberge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdarg.h>
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-
 int		ft_printf(char *s, ...);
 int		parse_string(va_list ap, char **to_print, int size);
+
+typedef struct	s_flags
+{
+	int			param;
+	int			fill_flag;
+	int			width;
+	int			precision;
+	char		flags;
+}				t_flags;
 
 #endif
