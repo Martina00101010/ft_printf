8a9d2a8b938422726875e9831b92a104d011ba88
Pypar Berge <pberge@oa-g5.21-school.ru>
2019-05-26 19:43:42 +0300

    params reduced to t_vaio

--- src/parsers.c
+++ src/parsers.c
@@ -6,26 +6,29 @@
 /*   By: pberge <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 12:35:59 by pberge            #+#    #+#             */
-/*   Updated: 2019/05/25 17:49:03 by pberge           ###   ########.fr       */
+/*   Updated: 2019/05/26 19:04:43 by pberge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "libftprintf.h"
 #include <stdlib.h>
 #include <stdarg.h>
 
-int		parse_string(va_list ap, char **to_print, int size)
+int		parse_string(t_vaio *v, t_flags flg)
 {
 	char	*sparam;
-	int		len;
+	int		slen;
+	int		cpylen;
 	char	*tmp;
 
-	sparam = va_arg(ap, char *);
-	len = ft_strlen(sparam);
-	tmp = *to_print;
-	*to_print = ft_strnew(size + len);
-	ft_strcat(*to_print, tmp);
-	ft_strcat(*to_print + size, sparam);
+	sparam = va_arg(v->ap, char *);
+	slen = ft_strlen(sparam);
+	cpylen = flg.width > slen ? flg.width : slen;
+	tmp = v->to_print;
+	v->to_print = ft_strnew(v->len + cpylen);
+	ft_strcat(v->to_print, tmp);
 	free(tmp);
-	return (len);
+	ft_strrcpy(v->to_print + v->len + cpylen, sparam + slen, slen + 1);
+	return (cpylen);
 }
