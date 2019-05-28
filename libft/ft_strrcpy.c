221fd902c9bcc03e0a4e1a403d89f7f5fbf1f47f
Pypar Berge <pberge@at-h6.21-school.ru>
2019-05-28 18:26:23 +0300

    coping string without \0

--- libft/ft_strrcpy.c
+++ libft/ft_strrcpy.c
@@ -6,14 +6,18 @@
 /*   By: pberge <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 16:24:01 by pberge            #+#    #+#             */
-/*   Updated: 2019/05/27 21:09:21 by pberge           ###   ########.fr       */
+/*   Updated: 2019/05/28 18:19:08 by pberge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** coping string backwards. not coping \0. filling remaining with spaces
+*/
+
 char	*ft_strrcpy(char *dst, char *src, int len)
 {
-	while (len-- > -1)
-		*(--dst) = *(--src);
+	while (len-- > 0)
+		*dst-- = *src--;
 	while (*dst == '\0')
 		*dst-- = ' ';
 	return (dst);
