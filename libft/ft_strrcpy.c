d0d204bb80ee15f40872c464fd7ebd20f075c082
Pypar Berge <pberge@at-h6.21-school.ru>
2019-05-28 18:54:10 +0300

    simplified view

--- libft/ft_strrcpy.c
+++ libft/ft_strrcpy.c
@@ -6,7 +6,7 @@
 /*   By: pberge <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 16:24:01 by pberge            #+#    #+#             */
-/*   Updated: 2019/05/28 18:19:08 by pberge           ###   ########.fr       */
+/*   Updated: 2019/05/28 18:37:14 by pberge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@
 char	*ft_strrcpy(char *dst, char *src, int len)
 {
 	while (len-- > 0)
-		*dst-- = *src--;
+	{
+		*dst = *src;
+		dst--;
+		src--;
+	}
 	while (*dst == '\0')
-		*dst-- = ' ';
+	{
+		*dst = ' ';
+		dst--;
+	}
 	return (dst);
 }
 
