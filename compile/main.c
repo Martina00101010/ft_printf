e2d93f1fa746c32bf54466fb294f8e75fdc8ec8e
Pypar Berge <pberge@oa-i4.21-school.ru>
2019-06-09 18:03:31 +0300

    handling percent

--- compile/main.c
+++ compile/main.c
@@ -6,7 +6,7 @@
 /*   By: pberge <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 21:19:55 by pberge            #+#    #+#             */
-/*   Updated: 2019/06/08 15:41:19 by pberge           ###   ########.fr       */
+/*   Updated: 2019/06/09 17:54:04 by pberge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,24 @@
 
 int		main(void)
 {
-	char	*s;
+	printf("|%d", ft_printf("%03%"));
+/*	short		si;
+	int			i;
+	signed char	sc;
+
+	sc = 1;
+	i = 12345;
+	si = 12345;
+	printf("%hi %hhi\n", si, sc);
+*/
+/*	char	*s;
 	char	*parse;
 
-	parse = "%.2s";
-//	s = "0123456789";
-	s = "this";
+	parse = "%021.5s";
+	s = "0123456789";
+//	s = NULL;
 	printf("%s\n", parse);
 	printf("|\t%i ft_printf\n", ft_printf(parse, s)); // my printf
 	   printf("|\t%i printf\n", printf(parse, s));    // libc printf
-	return (0);
+*/	return (0);
 }
